d9ec43882c06818db3be04deea9346b91f310def
EENADU <zero21code@gmail.com>
2023-02-24 23:47:24 +0530

    Code Improved and Customized.

--- app/src/main/java/com/example/firstapplication/CalculateBMI.kt
+++ app/src/main/java/com/example/firstapplication/CalculateBMI.kt
@@ -12,24 +12,35 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import com.example.firstapplication.databinding.MainActivityBinding
 
 class CalculateBMI: AppCompatActivity(), OnClickListener {
-    val tAG: String="BMI_Calculator"
-//globally assign
+    private val tAG: String="BMI_Calculator"
+/*//globally assign
     private lateinit var weight:EditText
     private lateinit var height:EditText
     private lateinit var button:Button
     private lateinit var result:TextView
     private lateinit var result2:TextView
-    private lateinit var context: Context
+    private lateinit var context: Context*/
+
+    //view binding created for main_activity for better performance
+    private lateinit var binding: MainActivityBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+
+        //inflating the layout
+        binding = MainActivityBinding.inflate(layoutInflater)
+
+        //set the content view as binding root
+        setContentView(binding.root)
 
         Log.d(tAG, "onCreate()")
 
         //taking values from user by ID and saving it
-        weight = findViewById(R.id.enterweight_1)
+        /*weight = findViewById(R.id.enterweight_1)
         height = findViewById(R.id.enterheight_1)
         button  = findViewById(R.id.Calculate_BMI)
         result = findViewById(R.id.result)
@@ -41,8 +52,14 @@ class CalculateBMI: AppCompatActivity(), OnClickListener {
         result2.visibility = View.INVISIBLE
 
         //on clicking the bmi calculator this functionality will run
-        button.setOnClickListener(this)
+        button.setOnClickListener(this)*/
 
+
+        binding.result.visibility = View.INVISIBLE
+        binding.messageUnderORoverWeight.visibility = View.INVISIBLE
+
+        //call the click listener
+        binding.CalculateBMI.setOnClickListener(this)
 
     }
     override fun onStart() {
@@ -76,7 +93,7 @@ class CalculateBMI: AppCompatActivity(), OnClickListener {
     }
 
     //created check_weight function to display result in result2
-    fun checkWeight(bmi: Float):String{
+    private fun checkWeight(bmi: Float):String{
         val message:String = if(bmi<18.5){
             "Underweight"
         } else if((bmi) in 18.5..24.9) {
@@ -89,7 +106,7 @@ class CalculateBMI: AppCompatActivity(), OnClickListener {
         return message // returning the message
     }
     //completion of work keyboard is hided
-    fun closeKeyboard() {
+    private fun closeKeyboard() {
         val newView: View? = this.currentFocus //created view to save the focus
         if(newView != null){
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -104,11 +121,11 @@ class CalculateBMI: AppCompatActivity(), OnClickListener {
                 var weightValue = 0.0 //assigning value
 
                 //if height and weight is not 0 then assigning the height and weight value
-                if(height.text.toString().isNotEmpty() && weight.text.toString().isNotEmpty()) {
-                    heightValue = height.text.toString().toDouble() //assigning height value
+                if(binding.enterheight1.text.toString().isNotEmpty() && binding.enterweight1.text.toString().isNotEmpty()) {
+                    heightValue = binding.enterheight1.text.toString().toDouble() //assigning height value
 
                     //if weight is not 0 then assigning the weight value
-                    weightValue = weight.text.toString().toDouble() //assigning weight value
+                    weightValue = binding.enterweight1.text.toString().toDouble() //assigning weight value
                 }
 
                 //calculating bmi and displaying it if height and weight is not 0
@@ -120,14 +137,14 @@ class CalculateBMI: AppCompatActivity(), OnClickListener {
                     val userBmi = String.format("%.2f", bmiValue).toFloat()
 
                     //displaying the result in result textview
-                    result.text = String.format("%.2f", bmiValue)
+                    binding.result.text = String.format("%.2f", bmiValue)
 
                     //calling the function check_weight to know whether bmi comes under normal underweight or overweight
-                    result2.text = "You are  ${checkWeight(userBmi)}"
+                    binding.messageUnderORoverWeight.text = "You are  ${checkWeight(userBmi)}"
 
                     //giving visibility to the textview
-                    result.visibility = VISIBLE
-                    result2.visibility = VISIBLE
+                    binding.result.visibility = VISIBLE
+                    binding.messageUnderORoverWeight.visibility = VISIBLE
 
                     //calling closeKeyboard function to close or hide after clicking the bmi calculator button (work completed)
                     closeKeyboard()
@@ -139,8 +156,8 @@ class CalculateBMI: AppCompatActivity(), OnClickListener {
                         Toast.LENGTH_LONG).show()
                 }
                 //after calculating and displaying the result clearing the edit_view of height and weight
-                weight.text.clear()
-                height.text.clear()
+                binding.enterweight1.text.clear()
+                binding.enterheight1.text.clear()
             }
         }
     }
