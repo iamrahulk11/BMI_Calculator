7bc1c60002781b8a1f2f5e9df80119fef7f87a31
EENADU <zero21code@gmail.com>
2023-03-02 16:42:49 +0530

    Code Improved and Customized.

--- app/src/main/java/com/example/firstapplication/CalculateBMI.kt
+++ app/src/main/java/com/example/firstapplication/CalculateBMI.kt
@@ -1,28 +1,31 @@
 package com.example.firstapplication
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.View.VISIBLE
-import android.view.ViewGroup
+
 import android.view.inputmethod.InputMethodManager
-import android.widget.*
-import androidx.appcompat.app.ActionBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firstapplication.databinding.MainActivityBinding
 
 class CalculateBMI: AppCompatActivity(), OnClickListener {
-    private val tAG: String="BMI_Calculator"
-/*//globally assign
+    private val tAG: String = "BMI_Calculator"
+
+    /*//globally assign
     private lateinit var weight:EditText
     private lateinit var height:EditText
     private lateinit var button:Button
     private lateinit var result:TextView
-    private lateinit var result2:TextView
-    private lateinit var context: Context*/
+    private lateinit var result2:TextView*/
+    private var check: Boolean = false
+    var heightValue = 0.0 //assigning value
+    var weightValue = 0.0 //assigning value
+    private lateinit var context: Context
 
     //view binding created for main_activity for better performance
     private lateinit var binding: MainActivityBinding
@@ -62,6 +65,7 @@ class CalculateBMI: AppCompatActivity(), OnClickListener {
         binding.CalculateBMI.setOnClickListener(this)
 
     }
+
     override fun onStart() {
         super.onStart()
         Log.d(tAG, "onStart()")
@@ -70,6 +74,7 @@ class CalculateBMI: AppCompatActivity(), OnClickListener {
     override fun onResume() {
         super.onResume()
         Log.d(tAG, "onResume()")
+
     }
 
     override fun onPause() {
@@ -93,73 +98,153 @@ class CalculateBMI: AppCompatActivity(), OnClickListener {
     }
 
     //created check_weight function to display result in result2
-    private fun checkWeight(bmi: Float):String{
-        val message:String = if(bmi<18.5){
+    private fun checkWeight(bmi: Float): String {
+        val message: String = if (bmi < 18.5) {
             "Underweight"
-        } else if((bmi) in 18.5..24.9) {
+        } else if ((bmi) in 18.5..24.9) {
             "Normal"
-        } else if(bmi in 24.9..29.9) {
+        } else if (bmi in 24.9..29.9) {
             "Overweight"
         } else {
             "Obese"
         }
         return message // returning the message
     }
+
     //completion of work keyboard is hided
     private fun closeKeyboard() {
         val newView: View? = this.currentFocus //created view to save the focus
-        if(newView != null){
+        if (newView != null) {
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(newView.windowToken, 0)
         }
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.Calculate_BMI -> {
-                var heightValue = 0.0 //assigning value
-                var weightValue = 0.0 //assigning value
 
-                //if height and weight is not 0 then assigning the height and weight value
-                if(binding.enterheight1.text.toString().isNotEmpty() && binding.enterweight1.text.toString().isNotEmpty()) {
-                    heightValue = binding.enterheight1.text.toString().toDouble() //assigning height value
+        if (!check) {
+            when (v?.id) {
+                R.id.Calculate_BMI -> {
+                    //if height and weight is not 0 and not empty then assigning the height and weight value
+                    if (binding.enterheight1.text.toString()
+                            .isNotEmpty() && binding.enterweight1.text.toString().isNotEmpty()
+                    ) {
+                        if(binding.enterheight1.text.toString().toInt() == 0 || binding.enterweight1.text.toString().toInt() == 0){
+                            if(binding.enterheight1.text.toString().toInt() == 0 && binding.enterweight1.text.toString().toInt() == 0){
+                                Toast.makeText(
+                                    this, "Weight and Height Value should be greater than 0",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                binding.enterweight1.text.clear()
+                                binding.enterheight1.text.clear()
+                                binding.enterweight1.requestFocus()
+                            }
+                            else if(binding.enterweight1.text.toString().toInt()==0){
+                                Toast.makeText(
+                                    this, "Weight Value should be greater than 0",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                binding.enterweight1.text.clear()
+                                binding.enterweight1.requestFocus()
+                            }else{
+                                Toast.makeText(
+                                    this, "Height Value should be greater than 0",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                binding.enterheight1.text.clear()
+                                binding.enterheight1.requestFocus()
+                            }
+                        }else {
+                            heightValue =
+                                binding.enterheight1.text.toString()
+                                    .toDouble() //assigning height value
 
-                    //if weight is not 0 then assigning the weight value
-                    weightValue = binding.enterweight1.text.toString().toDouble() //assigning weight value
+                            //if weight is not 0 then assigning the weight value
+                            weightValue =
+                                binding.enterweight1.text.toString()
+                                    .toDouble() //assigning weight value
+                        }
+                    }else{
+                        if (binding.enterheight1.text.toString().isEmpty() && binding.enterweight1.text.toString().isEmpty() ) {
+                            Toast.makeText(
+                                this, "Weight and Height Values should not be empty",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            binding.enterweight1.requestFocus()
+                            /*binding.enterheight1.text.clear()
+                            binding.enterweight1.text.clear()*/
+                        }
+                        else if (binding.enterheight1.text.toString()
+                                .isEmpty()) {
+                            Toast.makeText(
+                                this, "Height Value should not be empty",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            binding.enterheight1.requestFocus()
+                        }else if(binding.enterweight1.text.toString().isEmpty()) {
+                            Toast.makeText(
+                                this, "Weight Value should not be empty",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            binding.enterweight1.requestFocus()
+                        }
+                    }
+                    //calculating bmi and displaying it if height and weight is not 0
+                    if(heightValue > 0 && weightValue > 0) {
+                        //calculating bmi using bmi formula with giving float values
+                        val bmiValue =
+                            weightValue.toFloat() / ((heightValue.toFloat() / 100) * (heightValue.toFloat() / 100))
+                        //converting the values to string
+                        val userBmi = String.format("%.2f", bmiValue).toFloat()
+
+                        //calling the function check_weight to know whether bmi comes under normal underweight or overweight
+                        val Bmimessage = "${checkWeight(userBmi)}"
+
+                        //giving visibility to the textview
+                        binding.result.visibility = VISIBLE
+                        binding.messageUnderORoverWeight.visibility = VISIBLE
+
+                        //displaying the result in result textview
+                        binding.result.text = "Your BMI is " + String.format("%.2f", bmiValue)
+                        binding.result.setBackgroundColor(Color.rgb(173,216,230))
+
+                        if(Bmimessage == "Normal"){
+                            binding.messageUnderORoverWeight.text = "You are  ${Bmimessage}"
+                            binding.messageUnderORoverWeight.setBackgroundColor(Color.GREEN)
+                        }else if(Bmimessage == "Underweight") {
+                            binding.messageUnderORoverWeight.text = "You are  ${Bmimessage}"
+                            binding.messageUnderORoverWeight.setBackgroundColor(Color.YELLOW)
+                        }else if(Bmimessage == "Overweight"){
+                            binding.messageUnderORoverWeight.text = "You are  ${Bmimessage}"
+                            binding.messageUnderORoverWeight.setBackgroundColor(Color.MAGENTA)
+                        }else if(Bmimessage == "Obese"){
+                            binding.messageUnderORoverWeight.text = "You are  ${Bmimessage}"
+                            binding.messageUnderORoverWeight.setBackgroundColor(Color.RED)
+                        }
+
+                        check = true
+                        binding.CalculateBMI.text = "CLEAR"
+                        binding.enterheight1.isEnabled = false
+                        binding.enterweight1.isEnabled = false
+                        binding.enterheight1
+                        //calling closeKeyboard function to close or hide after clicking the bmi calculator button (work completed)
+                        closeKeyboard()
+                    }
+                    //if height and weight is 0 and still click the bmi calculator button this message will arise(pop up)
                 }
-
-                //calculating bmi and displaying it if height and weight is not 0
-                if(heightValue > 0 && weightValue > 0) {
-                    //calculating bmi using bmi formula with giving float values
-                    val bmiValue =
-                        weightValue.toFloat() / ((heightValue.toFloat() / 100) * (heightValue.toFloat() / 100))
-                    //converting the values to string
-                    val userBmi = String.format("%.2f", bmiValue).toFloat()
-
-                    //displaying the result in result textview
-                    binding.result.text = String.format("%.2f", bmiValue)
-
-                    //calling the function check_weight to know whether bmi comes under normal underweight or overweight
-                    binding.messageUnderORoverWeight.text = "You are  ${checkWeight(userBmi)}"
-
-                    //giving visibility to the textview
-                    binding.result.visibility = VISIBLE
-                    binding.messageUnderORoverWeight.visibility = VISIBLE
-
-                    //calling closeKeyboard function to close or hide after clicking the bmi calculator button (work completed)
-                    closeKeyboard()
-                }
-                //if height and weight is 0 and still click the bmi calculator button this message will arise(pop up)
-                else{
-                    Toast.makeText(
-                        this, "Please input Weight and Height Values greater than 0",
-                        Toast.LENGTH_LONG).show()
-                }
-                //after calculating and displaying the result clearing the edit_view of height and weight
+            }
+        }else{
                 binding.enterweight1.text.clear()
                 binding.enterheight1.text.clear()
-            }
-        }
-    }
+                binding.result.visibility = View.INVISIBLE
+                binding.messageUnderORoverWeight.visibility = View.INVISIBLE
+                binding.CalculateBMI.text = "CALCULATE"
+                check=false
+                binding.enterheight1.isEnabled = true
+                binding.enterweight1.isEnabled = true
+                binding.enterweight1.requestFocus()
 
+            }
+
+        }
 }
